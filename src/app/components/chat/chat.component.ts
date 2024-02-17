55cf6bc8d065073245f4ca2ebbcfeabdee980787
DEIVA SIVAM <115927155+asdeiva@users.noreply.github.com>
2024-02-17 12:49:24 +0530

    chat box created

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-chat',
@@ -6,10 +7,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent implements OnInit {
+  
+  @ViewChild('messageContainer') messageContainer: ElementRef | undefined;
+  @ViewChild('messageInput') messageInput: ElementRef | undefined;
+  
+  messages:any[]=[];
+  chatForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    private renderer: Renderer2
+    ) {
+    this.chatForm = this.formBuilder.group({
+      message: [''] 
+    });
   }
 
+  ngOnInit(): void {
+    this.onInit()
+  }
+  sendMessage(): void {
+    if (this.chatForm.valid) {
+      const message = this.chatForm.value.message;
+      this.messages.push(message); 
+      this.chatForm.reset();
+      this.scrollMessageIntoView();
+    }
+  }
+
+  scrollMessageIntoView(): void {
+    setTimeout(() => {
+      if (this.messageContainer && this.messageContainer.nativeElement) {
+        this.messageContainer.nativeElement.scrollTop = this.messageContainer.nativeElement.scrollHeight;
+      }
+    });
+  }
+  onInit(){
+    this.chatForm = this.formBuilder.group({
+      message: ['', Validators.required] 
+    });
+    if (this.messageInput && this.messageInput.nativeElement) {
+      this.renderer.selectRootElement(this.messageInput.nativeElement).focus();
+    }
+  }
 }
